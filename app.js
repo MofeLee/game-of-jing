d72bd07108dd02582f974a6e8e5f42b065a325ff
Mofe Lee <mofe@me.com>
2015-09-13 10:23:37 +0800

    release 1.0

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//  配置
 var config = {
   // 地图设置
   blocksNum: 3,
@@ -12,7 +13,8 @@ var config = {
 };
 
 var state =  {
-  isWhite: true
+  isWhite: true,
+  cheesemanCount: 0
 };
 
 var eles = {
@@ -20,7 +22,7 @@ var eles = {
   blocks: null // 放置方块元素
 };
 
-// 文档载入完成后执行
+//////////////// 文档载入完成后执行
 function load() {
   settingUpGame(eles.game, config);
 
@@ -34,8 +36,9 @@ function load() {
     }
   }
 
-}
+  eles.blocks = eles.game.childNodes;
 
+}
 window.onload = load;
 
 /////// 封装后的工具方法
@@ -73,6 +76,7 @@ function createBlock(size, margin, styles) {
   return block;
 }
 
+// 放置棋子事件
 function dropCheeseman() {
   // 判断是否已有棋子
   if(this.innerText){
@@ -87,7 +91,89 @@ function dropCheeseman() {
     this.innerText = config.black;
   }
 
+  // 判断胜负
+  if(isWin(this)){
+    var thisCheeseman = state.isWhite? '白旗':'黑棋' ;
+    alert(thisCheeseman + '获胜！');
+    resetGame();
+    return;
+  }
+
+  state.cheesemanCount++;
+  isMaxCheesemanCount();
+
   state.isWhite = !state.isWhite;
+}
+
+function isWin(that){
+  var cheeseman = that.innerText;
+  if (match3x3Pattern(cheeseman)){
+      return true;
+  }
+
+  return false;
+}
+
+// 临时算法,如果有时间可以改进 穷举8种情况
+function match3x3Pattern(cheeseman){
+  var winPosition = [
+    [[0,0], [0,1], [0,2]],
+    [[1,0], [1,1], [1,2]],
+    [[2,0], [2,1], [2,2]],
+
+    [[0,0], [1,0], [2,0]],
+    [[0,1], [1,1], [2,1]],
+    [[0,2], [1,2], [2,2]],
+
+    [[0,0], [1,1], [2,2]],
+    [[0,2], [1,1], [2,0]]
+  ];
+  for(var posIndex in winPosition){
+    var posArray = winPosition[posIndex];
+    if(match3Position(cheeseman, posArray)){
+      return true;
+    }
+  }
+  return false;
+}
+
+// example: matchPosition(black, [[1,1], [1,2], [1,3]])
+function match3Position(cheeseman, poses){
+  var mp = matchPosition;
+  if (mp(cheeseman, poses[0])
+      & mp(cheeseman, poses[1])
+      & mp(cheeseman, poses[2])){
+        return true;
+  }
+  return false;
+}
+
+function matchPosition(cheeseman, pos){
+  var block = eles.blocks[getCheeseIndex(pos)];
+
+  return block.innerText === cheeseman;
+
+}
+
+function getCheeseIndex(pos){
+  var index = pos[0] * config.blocksNum + pos[1];
+  return index + 1; // offset 1
+}
+
+// 判断棋子是否已满
+function isMaxCheesemanCount(){
+  var maxCheesemanCount = config.blocksNum * config.blocksNum;
+  state.maxCheesemanCount = maxCheesemanCount;
+  if(state.cheesemanCount >= maxCheesemanCount){
+    alert('棋盘已满，游戏结束');
+  }
+}
+
+function resetGame(){
+  for(i=0; i<state.maxCheesemanCount; i++){
+    eles.blocks[i+1].innerText = '';
+  }
+  state.cheesemanCount = 0;
 }
 
 ////// helper方法
